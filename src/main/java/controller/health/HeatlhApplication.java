eec5cd8bdb24b65634cc17a3512a0fc00902a1b2
Esteban297 <estebancristancho9@gmail.com>
2023-02-01 20:52:39 -0500

    first commit

---
+++ src/main/java/controller/health/HeatlhApplication.java
@@ -0,0 +1,13 @@
+package src.main.java.controller.heatlh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeatlhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeatlhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/controller/heatlh/HeatlhApplicationTests.java
@@ -0,0 +1,13 @@
+package src.main.java.controller.heatlh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeatlhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
